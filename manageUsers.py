f57422e1febf74bd160581b68c568ea8ccfb5fea
Dennis Maldonado <dennismald@gmail.com>
2015-10-31 15:48:30 +0000

    Adding initial project code

---
+++ manageUsers.py
@@ -0,0 +1,96 @@
+import string, random, sys
+
+authFile='visitors.txt'
+protocol='http://'
+hostname=''
+authParameterName='visitor'
+
+def constructAuth():
+    potentialData='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890'
+    auth=''.join(random.choice(potentialData) for _ in xrange(16))
+    return auth
+
+def addUser(name):
+    with open(authFile, 'a') as f:
+        auth=constructAuth()
+        f.write(name + ':' + auth + '\n')
+        print(name + ' Added to the file with auth key ' + auth)
+        return auth
+
+def readFileContents():
+    with open(authFile, 'r') as f:
+        fileContents=f.readlines()
+        return fileContents
+
+def searchForUser(name, userList):
+    match = 'noAuthCodeFound'
+    for item in userList:
+        if name.lower() in item.lower():
+            match = item.split(':')[1]
+            match = match[:-1]
+            break
+    return match
+
+def removeFromFile(authCode):
+    with open(authFile, 'r+') as f:
+        contents = f.readlines()
+        f.seek(0)
+        for item in contents:
+            if authCode not in item:
+                f.write(item)
+        f.truncate()
+
+if __name__ == '__main__':
+    if(len(sys.argv) == 1):
+        print('Add User:    python manageUsers.py add <name>')
+        print('Delete User: python manageUsers.py del <name>')
+        print('Show User:   python manageUsers.py shw <name>')
+
+    else:
+        if(sys.argv[1] == 'add'):
+            if(len(sys.argv) == 2):
+                name=raw_input('What is the name of the new user? ')
+            else:
+                name=''
+                i=2
+                for arg in sys.argv[2:]:
+                    name=name + ' ' + arg
+                name=name[1:]
+            authSearched=searchForUser(name, readFileContents())
+            if(authSearched != 'noAuthCodeFound'):
+                print('User already exists, try this argument: manageUsers.py shw ' + name)
+            else:
+                auth=addUser(name)
+                print(name + '\'s link is: ' + protocol + hostname + '/?' + authParameterName + '=' + auth)
+
+        elif(sys.argv[1] == 'del'):
+            if(len(sys.argv) == 2):
+                name=raw_input('Who should be removed from the list? ')
+            else:
+                name=''
+                i=2
+                for arg in sys.argv[2:]:
+                    name=name + ' ' + arg
+                name=name[1:]
+            authSearched=searchForUser(name, readFileContents())
+            if(authSearched != 'noAuthCodeFound'):
+                removeFromFile(authSearched)
+                print('Removed ' + name + ' from the authorization file')
+            else:
+                print('No Match found. Please  try another name.')
+
+
+        elif(sys.argv[1] == 'shw'):
+            if(len(sys.argv) == 2):
+                name=raw_input('Show which user? ')
+            else:
+                name=''
+                i=2
+                for arg in sys.argv[2:]:
+                    name=name + ' ' + arg
+                name=name[1:]
+            authSearched=searchForUser(name, readFileContents())
+            if(authSearched != 'noAuthCodeFound'):
+                print(name + '\'s link is: ' + protocol + hostname + '/?' + authParameterName + '=' + authSearched)
+            else:
+                print('No Match found. Please  try another name.')
---
+++ web_server.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python
+# Remote Gate Opener
+#
+# Made by Dennis <dennismald@gmail.com>
+# Todo:
+#   Implement logging
+#   Implement rate limiting
+#   Implement logging of times accessed
+#   Implement config file usage
+
+
+# Used for the HTTP Server
+import BaseHTTPServer
+import SocketServer
+
+# Libraries for interfacing with Raspberry Pi GPIO Ports
+import RPi.GPIO as GPIO
+
+import time
+
+HOST = ""
+PORT = 80
+PARAMETER = "visitor"
+GPIO_PIN = 17
+
+# Set up GPIO pins for Raspberry Pi
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(GPIO_PIN, GPIO.OUT)
+
+# Function to grab the parameter value from the URL 
+def get_parameter_value(url):
+    if "?" in url:
+        query = url.split("?")[1]
+        if "=" in query:
+            param,value = query.split("=")
+            if param == PARAMETER:
+                authenticate_value(value)
+                return
+            else:
+                print "[!] Invalid parameter"
+                return
+        else:
+            print "[!] Invalid query string"
+            return
+    else:
+        print "[!] No query string found"
+        return
+
+# Function to authenticate the value with a list of valid visitors
+def authenticate_value(v):
+    visitors = open("visitors.txt")
+    for record in visitors:
+        record = record.replace("\n", "")
+        name,key = record.split(":")
+        if v == key:
+            print "[*] Opening gate for " + name
+            push_button()
+            visitors.close()
+            return
+
+    print "[!] No matching keys found"
+    visitors.close()
+    return
+
+# Interface with the Raspberry Pi to perform the actions
+def push_button():
+    #RaspPi code to push the button
+    GPIO.output(GPIO_PIN, True)
+    time.sleep(2)
+    GPIO.output(GPIO_PIN, False)
+    return
+
+# Class for Web Server
+class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+
+    # Respond to GET requests
+    def do_GET(s):
+        s.send_response(200)
+        s.end_headers() 
+        s.wfile.write("<center><h1>Welcome!</h1></center>")
+        get_parameter_value(s.path)
+
+    # Hide HTTP request log from stdout
+    def log_message(self, format, *args):
+        return
+
+if __name__ == '__main__':
+    server_class = BaseHTTPServer.HTTPServer
+    httpd = server_class((HOST, PORT), MyHandler)
+    print "[*] Starting Web Server on:"
+    print "    Host: " + HOST
+    print "    Port: " + str(PORT)
+    print "----------------------"
+    print ""
+
+try:
+    httpd.serve_forever()
+except (KeyboardInterrupt):
+    print " Keyboard Interrupt detected..."
+    print "[!] Closing Server..."
+    print ""
+
+GPIO.cleanup()
+httpd.server_close()
