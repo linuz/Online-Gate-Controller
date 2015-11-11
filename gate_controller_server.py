2f0c911bc9388b5d518a7e17aef737235528a175
Dennis Maldonado <dennismald@gmail.com>
2015-11-10 20:13:14 -0600

    Bug fixes

--- gate_controller_server.py
+++ gate_controller_server.py
@@ -24,7 +24,8 @@ from time import strftime, localtime
 settings = ConfigParser.RawConfigParser()
 visitors = ConfigParser.RawConfigParser()
 settings.read("settings.cfg")
-host = settings.get("Web Server", "Hostname")
+#host = settings.get("Web Server", "Hostname")
+host = "0.0.0.0"
 port = settings.getint("Web Server", "Port")
 parameter = settings.get("Web Server", "Parameter")
 response_html = settings.get("Web Server", "Response_HTML")
@@ -34,14 +35,6 @@ visitor_file = settings.get("File", "Visitor_File")
 web_server_access_log_file = settings.get("Logs", "Web_Server_Access_Log")
 web_server_error_log_file = settings.get("Logs", "Web_Server_Error_Log")
 visitor_log_file = settings.get("Logs", "Visitor_Log")
-
-#Web Server config
-cherrypy.config.update({'server.socket_host': host,
-                        'server.socket_port': port,
-                        'environment': 'production',
-                        'log.access_file': web_server_access_log_file,
-                        'log.error_file': web_server_error_log_file
-                        })
 
 # Check for exisitance of visitor file
 if not os.path.exists(visitor_file):
@@ -58,6 +51,14 @@ if "/" in web_server_error_log_file:
 if "/" in visitor_log_file:
     if not os.path.exists(os.path.dirname(visitor_log_file)):
         os.makedirs(os.path.dirname(visitor_log_file))
+
+#Web Server config
+cherrypy.config.update({'server.socket_host': host,
+                        'server.socket_port': port,
+                        'environment': 'production',
+                        'log.access_file': web_server_access_log_file,
+                        'log.error_file': web_server_error_log_file
+                        })
 
 # Set up GPIO pins for Raspberry Pi
 gpio_pin = settings.getint("Raspberry Pi", "GPIO_Pin")
@@ -96,5 +97,8 @@ class WebServer(object):
         return response_html
 
 if __name__ == '__main__':
+    print ""
+    print "Starting Web Server on 0.0.0.0:" + str(port)
+    print ""
     cherrypy.quickstart(WebServer())
     GPIO.cleanup()
